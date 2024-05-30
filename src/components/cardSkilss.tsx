696f6bc1f7f03ae4965086ddf6d5d2ed6e3d753f
Cucumberchik <durovalexei93@gmail.com>
2024-05-30 11:43:01 +0600

    added main

---
+++ src/components/cardSkilss.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+import { ReactNode } from 'react'
+
+const CardSkilss:NextPage<{img: string, title: string, description: string}> = 
+({img, title, description}):ReactNode => {
+  return (
+    <div className='card_skils'>
+        
+    </div>
+  );
+};
+
+export default CardSkilss;
---
+++ src/components/skils.tsx
@@ -0,0 +1,16 @@
+
+
+import { NextPage } from 'next';
+import { ReactNode } from 'react'
+
+const Skils:NextPage = ():ReactNode => {
+  return (
+    <section id='skils'>
+        <div className="container">
+            
+        </div>
+    </section>
+  );
+};
+
+export default Skils;
